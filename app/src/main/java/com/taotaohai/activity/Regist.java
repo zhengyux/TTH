921cfa891e150efebde2368bfc2bffb85d285762
lapisdm <954955937@qq.com>
2017-09-26 11:18:23 +0800

    增加注册界面

---
+++ app/src/main/java/com/taotaohai/activity/Regist.java
@@ -0,0 +1,15 @@
+package com.taotaohai.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.taotaohai.R;
+
+public class Regist extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_regist);
+    }
+}
